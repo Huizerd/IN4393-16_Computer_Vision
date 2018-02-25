0512d8516de31751ad87c1365842ecfdeab2565a
MichielJM <michielmollema@gmail.com>
2018-02-25 16:32:59 +0100

    Added variable filter size, our filter should now be the same as standard Matlab implementation for all sigmas

--- 1_LinearFilters/gaussian.m
+++ 1_LinearFilters/gaussian.m
@@ -2,7 +2,8 @@
 
 function G = gaussian(sigma)
 
-x = linspace(-3*sigma, 3*sigma, 9);
+sz = floor(3*sigma + 0.5);
+x = linspace(-3*sigma, 3*sigma, (2*sz+1));
 
 G = 1 / (sigma * sqrt(2*pi)) * exp(-x.*x / (2*sigma*sigma));
 

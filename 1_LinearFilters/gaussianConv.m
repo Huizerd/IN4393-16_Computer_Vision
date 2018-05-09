88148ba9005bca2b4a7c99bc2db38d802ea425bf
Jesse Hagenaars <jessehagenaars@gmail.com>
2018-05-10 00:06:54 +0200

    New ignore

--- 1_LinearFilters/gaussianConv.m
+++
@@ -1,8 +0,0 @@
-function imOut = gaussianConv(image, sigma_x, sigma_y)
-
-u = gaussian(sigma_x);
-v = gaussian(sigma_y);
-
-imOut = conv2(u, v, image, 'same');
-
-end
--- 4_Optical_Flow_SfM/gaussianDer.m
+++
@@ -1,8 +0,0 @@
-function Gd = gaussianDer(G, sigma)
-
-sz = floor(3*sigma + 0.5);
-x = linspace(-3*sigma, 3*sigma, (2*sz+1));
-
-Gd = -x ./ (sigma*sigma) .* G;
-
-end

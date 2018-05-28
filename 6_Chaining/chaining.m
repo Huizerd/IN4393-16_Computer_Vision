c95859064d2d378ccc1691a394fd811fd61d5986
Huizerd <jessehagenaars@gmail.com>
2018-05-28 10:11:45 +0200

    Chaining function

---
+++ 6_Chaining/chaining.m
@@ -0,0 +1,8 @@
+function point_view_matrix = chaining(matches)
+% CHAINING Constructs the point-view matrix with the matches found between
+% consecutive frames.
+%
+% matches: matrix containing
+
+
+end

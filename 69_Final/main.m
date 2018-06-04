5761ccfff65cfd7f227165aa40867c8772620d36
Huizerd <jessehagenaars@gmail.com>
2018-06-04 09:12:30 +0200

    Init final project

---
+++ 69_Final/main.m
@@ -0,0 +1,27 @@
+% IN4393-16: Final Assignment
+% 
+% Jesse Hagenaars & Michael Mollema - 04.06.2018
+
+%% Load data
+
+data_location = 'model_castle';
+
+%% Feature point detection & extraction of SIFT descriptors (4 pts)
+
+
+%%  Normalized 8-point RANSAC to find best matches (4 pts)
+
+
+%% Chaining (8 pts)
+
+
+%% Stitching (12 pts)
+
+
+%% Bundle adjustment (4 pts)
+
+
+%% Eliminate affine ambiguity (4 pts)
+
+
+%% 3D model plotting (4 pts)

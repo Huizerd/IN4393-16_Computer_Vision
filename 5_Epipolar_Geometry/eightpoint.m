4cdde70ea675f07ad820f4756259ec1e049b9eb3
MichielJM <michielmollema@gmail.com>
2018-06-11 09:31:50 +0200

    Working 8point algorithm (could use cleanup I guess)

--- 5_Epipolar_Geometry/eightpoint.m
+++ 5_Epipolar_Geometry/eightpoint.m
@@ -3,7 +3,22 @@
 % Jesse Hagenaars & Michiel Mollema - 28-05-2018
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [f_best, inliers_best] = eightpoint(A, threshold)
+function [F_best, F_ransac_denorm, inliers_best] = eightpoint(x1, y1, x2, y2, best_matches, threshold)
+
+% Create matrix A
+A = zeros(length(best_matches), 9);
+
+for row = 1:size(A, 1)
+    A(row,:) = [x1(:,best_matches(2,row))*x2(:,best_matches(3,row))
+                x1(:,best_matches(2,row))*y2(:,best_matches(3,row))
+                x1(:,best_matches(2,row))
+                y1(:,best_matches(2,row))*x2(:,best_matches(3,row))
+                y1(:,best_matches(2,row))*y2(:,best_matches(3,row))
+                y1(:,best_matches(2,row))
+                x2(:,best_matches(3,row))
+                y2(:,best_matches(3,row))
+                1];
+end
 
 [~, ~, Va]      = svd(A); % Singular Value decomposition of A
 f               = Va(:,end); % Last column of V (corresponding to smallest singular value equals f
@@ -12,7 +27,7 @@ F               = reshape(f, 3, 3); % Reshape f to matrix F
 % Enforce singularity of fundamental matrix F
 [Uf, Df, Vf]    = svd(F);
 Df(end, end)    = 0;
-F               = Uf * Df * Vf.';
+F               = Uf * Df * Vf';
 
 %% Normalization
 % For  first image
@@ -22,10 +37,10 @@ p       = [xcoords, ycoords, ones(size(xcoords))]';
 mx  = mean(xcoords);
 my  = mean(ycoords);
 d   = mean(sqrt((xcoords-mx).^2 + (ycoords-my).^2));
-T   = [[sqrt(2)/d, 0, -mx * sqrt(2)/d];...
+T1   = [[sqrt(2)/d, 0, -mx * sqrt(2)/d];...
         [0, sqrt(2)/d, -my * sqrt(2)/d];...
         [0, 0, 1]];
-p_hat = T * p;
+p_hat = T1 * p;
 
 % For second image
 xcoords = A(:,7);
@@ -34,10 +49,10 @@ p_acc   = [xcoords, ycoords, ones(size(xcoords))]';
 mx  = mean(xcoords);
 my  = mean(ycoords);
 d   = mean(sqrt((xcoords-mx).^2 + (ycoords-my).^2));
-T   = [[sqrt(2)/d, 0, -mx * sqrt(2)/d];...
+T2   = [[sqrt(2)/d, 0, -mx * sqrt(2)/d];...
         [0, sqrt(2)/d, -my * sqrt(2)/d];...
         [0, 0, 1]];
-p_hat_acc = T * p_acc;
+p_hat_acc = T2 * p_acc;
 
 %% RANSAC
 n_inliers_best = 0;
@@ -47,7 +62,7 @@ for n = 1:N
     perm = randperm(length(xcoords));
     P    = 8;
     seed = perm(1:P);
-    p_hat_used = p_hat(:, seed);
+    p_hat_used = p_hat (:, seed);
     p_hat_acc_used = p_hat_acc(:,seed);
     
     % Create new A matrix from normalized coordinates
@@ -64,18 +79,26 @@ for n = 1:N
                 1];
     end
     
-    % Determine fundamental matrix F
-    [V, ~]   = eigs(Aransac' * Aransac, 1, 'smallestabs');
-    f_ransac = V;
-    F_ransac = reshape(f_ransac, 3, 3);
+%     % Determine fundamental matrix F
+%     [V, ~]   = eigs(Aransac' * Aransac, 1, 'smallestabs');
+%     f_ransac = V;
+%     F_ransac = reshape(f_ransac, 3, 3);
+    [~, ~, Vransac]        = svd(Aransac); % Singular Value decomposition of A
+    f_ransac               = Vransac(:,end); % Last column of V (corresponding to smallest singular value equals f
+    F_ransac               = reshape(f_ransac, 3, 3); % Reshape f to matrix F
+    
+    % Force singularity in F
+    [Uf, Df, Vf] = svd(F_ransac);
+    Df(end, end) = 0;
+    F_ransac     = Uf * Df * Vf';
     
     % Find inliers for all n points in A
-    Fp = F_ransac * p_hat_used;
-    Ftp= F_ransac'* p_hat_used;
-    d   = zeros(1, length(Fp));
+    Fp = F_ransac * p_hat;
+    Ftp= F_ransac'* p_hat;
+    d  = zeros(1, length(p_hat));
     
-    for i = 1:length(Fp)
-        d(i) = (p_hat_acc_used(:,i)' * F_ransac * p_hat_used(:,i))^2 / ...
+    for i = 1:length(p_hat)
+        d(i) = (p_hat_acc(:,i)' * F_ransac * p_hat(:,i))^2 / ...
             (Fp(1,i)^2 + Fp(2,i)^2 + Ftp(1,i)^2 + Ftp(2,i)^2);
     end
 
@@ -85,8 +108,11 @@ for n = 1:N
     if n_inliers > n_inliers_best   
         n_inliers_best   = n_inliers;
         inliers_best     = inliers;
-        f_best           = F_ransac;  
+        F_best           = F_ransac;  
+%         F_best = F;
     end
 end
+
+F_ransac_denorm = T1' * F_best * T1;
 
 end

55e99163945a0859b781960b0f28a3e968b9eabc
Huizerd <jessehagenaars@gmail.com>
2018-06-20 16:02:54 +0200

    Procrustes working

--- 69_Final/get_sift_harris.m
+++ 69_Final/get_sift_harris.m
@@ -1,18 +1,18 @@
-function [features, descriptors] = get_sift_harris(image, sigma, threshold_R)
+function [features, descriptors] = get_sift_harris(edges, image, sigma, threshold_R)
 
 % Derivative scale = gamma * integration scale
 gamma = 0.7;
     
 % To store Laplacian
-laplacian = zeros([size(image) length(sigma)]);
+laplacian = zeros([size(edges) length(sigma)]);
 
 for s = 1:length(sigma)
 
     % Get locations of Harris corners
-    [r, c] = harris(image, sigma(s), threshold_R);
+    [r, c] = harris(edges, sigma(s), threshold_R);
 
     % Calculate Laplacian
-    laplacian_i = sigma(s)^2 * (imageDerivatives(image, sigma(s)*gamma, 'xx') + imageDerivatives(image, sigma(s)*gamma, 'yy'));
+    laplacian_i = sigma(s)^2 * (imageDerivatives(edges, sigma(s)*gamma, 'xx') + imageDerivatives(edges, sigma(s)*gamma, 'yy'));
 
     % Put in store
     laplacian(sub2ind(size(laplacian), r, c, ones(size(r)) * s)) = abs(laplacian_i(sub2ind(size(laplacian_i), r, c)));
@@ -30,7 +30,7 @@ max_laplacian_clean = (max_laplacian_block == max_laplacian) & (max_laplacian > 
 [r, c] = find(max_laplacian_clean);
 
 % Store sigmas for max responses
-sigma_max = zeros(size(image));
+sigma_max = zeros(size(edges));
         
 for s = 1:length(sigma)
     
--- 69_Final/main_new.m
+++ 69_Final/main_new.m
@@ -36,8 +36,7 @@ for i = 1:length(image_files)
     end
     
     images(:, :, :, i) = uint8(current_image);
-    images_gray(:, :, i) = rgb2gray(current_image);
-    
+    images_gray(:, :, i) = rgb2gray(current_image);   
    
 end
 
@@ -45,7 +44,7 @@ end
 %% Feature point detection & extraction of SIFT descriptors (4 pts)
 
 % Only do if file doesn't exist already
-if ~exist('features/sift.mat', 'file')
+if ~exist('features/sift_edges.mat', 'file')
     
     sift = {};  
 
@@ -53,21 +52,34 @@ if ~exist('features/sift.mat', 'file')
         
         fprintf('Finding corners with Harris & detecting SIFT, image %d\n', i)
         
+        % Gaussian filter to smooth image
+        % smooth = uint8(conv2(gaussian(3), gaussian(3), images_gray(:, :, i), 'same'));
+        
+        % Detect edges
+        edges = edge(images_gray(:, :, i), 'Sobel');
+        
         % Get SIFT features based on Harris corners
         % [features, descriptors]
-        [sift{1, i}, sift{2, i}] = get_sift_harris(images_gray(:, :, i), sigma, threshold_R);
+        [sift{1, i}, sift{2, i}] = get_sift_harris(edges, images_gray(:, :, i), sigma, threshold_R);
+        
+        % Also get VLFeat SIFT and concatenate
+        % Potentially many false matches? --> allright
+        [sift_vlfeat_f, sift_vlfeat_d] = vl_sift(single(images_gray(:, :, i)));
+        
+        sift{1, i} = [sift{1, i} sift_vlfeat_f];
+        sift{2, i} = [sift{2, i} sift_vlfeat_d];
 
     end
     
     % Save
-    save('features/sift', 'sift')
+    save('features/sift_edges', 'sift')
     
 else
     
     disp('Loading features...')
     
     % Load already detected features
-    load('features/sift', 'sift')
+    load('features/sift_edges', 'sift')
     
 end
 
@@ -75,7 +87,7 @@ end
 %%  Normalized 8-point RANSAC to find best matches (4 pts)
 
 % Only do if file doesn't exist already
-if ~exist('matches/matches_8pt_RANSAC.mat', 'file')
+if ~exist('matches/matches_8pt_RANSAC_125_10_edges.mat', 'file')
     
     % Cell array of matches per frame pair
     matches_8pt_RANSAC = {};
@@ -89,27 +101,21 @@ if ~exist('matches/matches_8pt_RANSAC.mat', 'file')
         [F_ransac_denorm, inliers_1, inliers_2, inliers_idx] = do_eightpoint(sift, match_threshold, dist_threshold, i);
         matches_8pt_RANSAC{1, i} = inliers_idx;
         
-        % Plot one of the 'problematic' images
-        if i == 1
-                            
-            showMatchedFeatures(images(:, :, :, i), images(:, :, :, i+1), inliers_1, inliers_2)
-            plot_eightpoint(images(:, :, :, i), images(:, :, :, i+1), inliers_1, inliers_2, F_ransac_denorm);
-            
+        if i ~= size(sift, 2)
+            figure;
+            showMatchedFeatures(images(:, :, :, i), images(:, :, :, i+1), inliers_1, inliers_2) 
         end
-
     end
 
-    save('matches/matches_8pt_RANSAC', 'matches_8pt_RANSAC')
+    save('matches/matches_8pt_RANSAC_125_10_edges', 'matches_8pt_RANSAC')
     
-else
-    
-    load('matches/matches_8pt_RANSAC', 'matches_8pt_RANSAC')
-    
+else   
+    load('matches/matches_8pt_RANSAC_125_10_edges', 'matches_8pt_RANSAC')   
 end
 
 % If you want to plot a specific pair
-% showMatchedFeatures(images(:, :, :, 15), images(:, :, :, 16), sift{1, 15}(1:2, matches_8pt_RANSAC{1, 15}(1, :))', sift{1, 16}(1:2, matches_8pt_RANSAC{1, 15}(2, :))')
-
+% showMatchedFeatures(images(:, :, :, 1), images(:, :, :, 2), sift{1, 1}(1:2, matches_8pt_RANSAC{1, 1}(1, :))', sift{1, 2}(1:2, matches_8pt_RANSAC{1, 1}(2, :))')
+% bound_x = [885 3286 3286 885 885]; bound_y = [726 726 1906 1906 726]; hold on; plot(bound_x, bound_y)
 
 %% Chaining (8 pts)
 
@@ -124,41 +130,49 @@ S = {};
 points_common = {};
 colors = {};
 
-% Use 4 consecutive frames each time
-for f = 0:size(point_view_matrix, 1) - 1
-    
-    % Shift (cell) array circularly (current set of frames)
-    pv_matrix_circ = circshift(point_view_matrix, -f, 1);
-    sift_circ = circshift(sift, -f, 2);
-    
-    % Shift one more (next set of frames)
-    pv_matrix_circ_next = circshift(point_view_matrix, -f-1, 1);
-    sift_circ_next = circshift(sift, -f-1, 2); 
-    
-    % Get x, y for each SIFT descriptor (for current & next set)
-    points = get_points(sift_circ(1, 1:4), pv_matrix_circ(1:4, :));
-    points_next = get_points(sift_circ_next(1, 1:4), pv_matrix_circ_next(1:4, :));
-    
-    % Next set is shifted by 1 w.r.t. current set, so look for points that
-    %   are present in the last 3 frames of current set, and first 3 frames
-    %   of next set --> you end up with points that were visible for 5
-    %   consecutive frames, which form the connection between both sets
-    % 1:end-2 and 3:end since rows are alternating x & y
-    [K, points_common{2, f+1}, points_common{1, f+1}] = intersect(points_next(1:end-2, :)', points(3:end, :)', 'rows');
-    
-    % Get color for later plotting
-    color = [images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)]))); ...
-              images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), 2*ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)]))); ...
-              images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), 3*ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)])))];
-    
-    % Only do if there are at least 3 points
-    if size(points, 2) > 2
-    
-        % Perform structure-from-motion and solve for affine ambiguity
-        S{1, f+1} = SfM(points);
-        colors{1, f+1} = color;
-        
+% Use n consecutive frames each time
+consec = [3 4];
+count = 0;
+
+for n = 1:length(consec)
+    for f = 0:size(point_view_matrix, 1) - 1
+
+        % Shift (cell) array circularly (current set of frames)
+        pv_matrix_circ = circshift(point_view_matrix, -f, 1);
+        sift_circ = circshift(sift, -f, 2);
+
+        % Shift one more (next set of frames)
+        pv_matrix_circ_next = circshift(point_view_matrix, -f-1, 1);
+        sift_circ_next = circshift(sift, -f-1, 2); 
+
+        % Get x, y for each SIFT descriptor (for current & next set)
+        points = get_points(sift_circ(1, 1:consec(n)), pv_matrix_circ(1:consec(n), :));
+        points_next = get_points(sift_circ_next(1, 1:consec(n)), pv_matrix_circ_next(1:consec(n), :));
+
+        % Next set is shifted by 1 w.r.t. current set, so look for points that
+        %   are present in the last 3 frames of current set, and first 3 frames
+        %   of next set --> you end up with points that were visible for 5
+        %   consecutive frames, which form the connection between both sets
+        % 1:end-2 and 3:end since rows are alternating x & y
+        [K, points_common{n+count+1, f+1}, points_common{n+count, f+1}] = intersect(points_next(1:end-2, :)', points(3:end, :)', 'rows');
+
+        % Get color for later plotting
+        color = [images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)]))); ...
+                 images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), 2*ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)]))); ...
+                 images(sub2ind(size(images), uint16(points(2, :)), uint16(points(1, :)), 3*ones([1, size(points, 2)]), f+1 * ones([1, size(points, 2)])))];
+
+        % Only do if there are at least 3 points
+        if size(points, 2) > 2
+
+            % Perform structure-from-motion and solve for affine ambiguity
+            S{n, f+1} = SfM(points);
+            colors{n, f+1} = color;
+
+        end
     end
+    
+    % Increment count to write in next 2 rows
+    count = count + 1;
     
 end
 
@@ -167,10 +181,9 @@ S_final = S{1, 1};
 colors_final = colors{1, 1};
 
 % Also try with pointCloud object
-scene = pointCloud(S{1, 1}', 'Color', uint8(colors{1, 1}'));
-merge_size = 0.15;
-
-pcshow(scene)
+inverted_scene = [S{1, 1}(1, :); -S{1, 1}(3, :); -S{1, 1}(2, :)]';
+scene = pointCloud(inverted_scene, 'Color', uint8(colors{1, 1}'));
+merge_size = 0.01;
 
 % Go over the sets
 for s = 0:size(S, 2) - 1
@@ -180,17 +193,21 @@ for s = 0:size(S, 2) - 1
     colors_circ = circshift(colors, -s, 2);
     
     % Minimum number of rows, to check if both sets > 0 points
-    min_cols = min(cellfun('size', S_circ(1, 1:2), 2));
+    % min_cols = min(cellfun('size', S_circ(1, 1:2), 2));
     
-    % Both sets need to have > 0 points
-    if min_cols > 0
+    % Common points has to be > 0
+    if ~isempty(points_common{1, s+1})
         
         % Save colors
         colors_final = [colors_final colors_circ{1, 2}];
         
+        % Plot matching points
+        view_1 = S_circ{1, 1}(:, points_common{1, s+1})';
+        view_2 = S_circ{1, 2}(:, points_common{2, s+1})';
+        
         % Get transformation between 3D point sets
         % Transpose since x, y, z have to be columns
-        [~, ~, tform] = procrustes(S_circ{1, 1}(:, points_common{1,s+1})', S_circ{1, 2}(:, points_common{2,s+1})');
+        [d, K, tform] = procrustes(view_1, view_2);
         
         % All rows are equal for c --> convert to 1 row to allow pointwise
         %   ops
@@ -199,20 +216,32 @@ for s = 0:size(S, 2) - 1
         % Do transform, see MATLAB's procrustes documentation
         Z = tform.b * S_circ{1, 2}' * tform.T + tform.c;
         
-        % Convert to pointCloud
-        new_cloud = pointCloud(Z, 'Color', uint8(colors_circ{1, 2}'));
+        % SAVE Z IN S FOR NEXT TRANSFORM
+        S{1, s+2} = Z';
         
-        % Extend final 3D point set --> check dimensions
-        S_final = [S_final Z'];
+        if d < 0.1
         
-        % Also merge clouds
-        scene = pcmerge(scene, new_cloud, merge_size);
-        
-        figure
-        pcshow(scene)
-        
-    end
-    
+            % Convert to pointCloud
+            new_cloud = pointCloud([Z(:, 1) -Z(:, 3) -Z(:, 2)], 'Color', uint8(colors_circ{1, 2}'));
+
+            % Extend final 3D point set --> check dimensions
+            S_final = [S_final Z'];
+
+            % Also merge clouds
+            scene = pcmerge(scene, new_cloud, merge_size);
+            
+            figure
+            subplot(1, 2, 1)
+            scatter3(view_1(:, 1), view_1(:, 2), view_1(:, 3), 'b.')
+            hold on
+            scatter3(view_2(:, 1), view_2(:, 2), view_2(:, 3), 'r.')
+            scatter3(K(:, 1), K(:, 2), K(:, 3), 'g.')
+            title(num2str(d))
+            subplot(1, 2, 2)
+            pcshow(scene, 'MarkerSize', 100)
+            
+        end    
+    end  
 end
 
 % Check for close points in some way? --> can be done using pcmerge
@@ -230,4 +259,4 @@ end
 
 %% 3D model plotting (4 pts)
 
-plot3(S_final(1, :), S_final(2, :), S_final(3, :), 'b.')
+pcshow(scene, 'MarkerSize', 100)

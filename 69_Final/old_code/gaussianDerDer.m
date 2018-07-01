e2e1930ea3ee1ec30db20ecbdc0fd8f36239f47e
Huizerd <jessehagenaars@gmail.com>
2018-07-01 22:37:35 +0200

    Deleted old code

--- 69_Final/old_code/gaussianDerDer.m
+++
@@ -1,8 +0,0 @@
-function Gdd = gaussianDerDer(G, sigma)
-
-sz = floor(3*sigma + 0.5);
-x = linspace(-3*sigma, 3*sigma, (2*sz+1));
-
-Gdd = (-(sigma*sigma) + (x.*x)) ./ (sigma*sigma*sigma*sigma) .* G;
-
-end
--- 69_Final/old_code/gradMag.m
+++
@@ -1,8 +0,0 @@
-function [Ix, Iy] = gradMag(image, sigma)
-
-G = gaussian(sigma);
-
-Ix = conv2(image, gaussianDer(G, sigma), 'same');
-Iy = conv2(image, gaussianDer(G, sigma)', 'same');
-
-end
